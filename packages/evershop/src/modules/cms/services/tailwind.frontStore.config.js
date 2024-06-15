bae24d1f5624b1abd8374149f8e5df9095dd7925
Selvaraj <selvaraj.manikkalai1995@gmail.com>
2024-06-15 12:57:00 +0530

    adding

---
+++ packages/evershop/src/modules/cms/services/tailwind.frontStore.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: 'Helvetica,Helvetica Neue,Arial,Lucida Grande,sans-serif'
+    },
+    fontSize: {
+      base: '.875rem'
+    }
+  }
+};
